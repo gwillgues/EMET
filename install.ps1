66ed24fcfcc8c5b19f059ac4db4242f04b63f1a7
gwillgues <96144967+gwillgues@users.noreply.github.com>
2023-05-05 23:31:23 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/gwillgues/EMET/raw/main/win_proc_mitigations.xml", "C:\Windows\Temp\win_proc_mitigations.xml")
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/gwillgues/Exploit-Guard-Config/raw/main/win_proc_mitigations.xml", "C:\Windows\Temp\win_proc_mitigations.xml")
 Set-ProcessMitigation -PolicyFilePath "C:\Windows\Temp\win_proc_mitigations.xml"
